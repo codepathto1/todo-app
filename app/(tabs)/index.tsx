9143033555bf87da4b03a8accd655f13594cdf98
iamalex11010 <iamalex11010@gmail.com>
2026-01-06 17:57:01 +0000

    Integrate convex and define functions and schema

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,27 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-const {toggleTheme}=useTheme()
+  const { toggleTheme } = useTheme();
+  const todos = useQuery(api.todos.getTodos);
+
+  console.log(todos);
+
+  const addTodo = useMutation(api.todos.addTodo);
+  const clearAll = useMutation(api.todos.deleteAllTodos);
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home</Text>
       <TouchableOpacity onPress={toggleTheme}>
         <Text style={styles.button}>Toggle Theme</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({ text: "Add todo" })}>
+        <Text style={styles.button}>Add Todo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => clearAll()}>
+        <Text style={styles.button}>Clear All</Text>
       </TouchableOpacity>
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,16 @@
 import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
 export default function RootLayout() {
   return (
+    <ConvexProvider client={convex}>
     <ThemeProvider>
       <Stack>
         <Stack.Screen
@@ -10,5 +19,6 @@ export default function RootLayout() {
         />
       </Stack>
     </ThemeProvider>
+    </ConvexProvider>
   );
 }
